b2b6d3c962e1632a03257957761ec2198baf65fb
Travis Vachon <travis.vachon@gmail.com>
2022-12-15 06:58:31 -0800

    add basic w3ui test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { KeyringProvider } from '@w3ui/react-keyring'
+import { UploaderProvider } from '@w3ui/react-uploader'
+import { UploadsListProvider } from '@w3ui/react-uploads-list'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <KeyringProvider>
+      <UploaderProvider>
+        <UploadsListProvider>
+          <Component {...pageProps} />
+        </UploadsListProvider>
+      </UploaderProvider>
+    </KeyringProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import { useKeyring } from '@w3ui/react-keyring'
+import { useUploader } from '@w3ui/react-uploader'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [, { uploadFile }] = useUploader()
+  const [{ space }, { createSpace, registerSpace }] = useKeyring()
+
+  async function login() {
+    await createSpace()
+    await registerSpace("travis@dag.house")
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +21,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        registered? {space?.registered()}
+        <button onClick={login}>log in</button>
       </main>
 
       <footer className={styles.footer}>
